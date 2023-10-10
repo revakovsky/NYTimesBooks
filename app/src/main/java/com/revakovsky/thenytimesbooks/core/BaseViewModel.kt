eb80d3516cdf8957e8f433d483f6476f8c95ac3b
@revakovsky <revakovsky@gmail.com>
2023-10-10 07:52:55 +0300

    improve base viewModel and connectivity check

--- app/src/main/java/com/revakovsky/thenytimesbooks/core/BaseViewModel.kt
+++ app/src/main/java/com/revakovsky/thenytimesbooks/core/BaseViewModel.kt
@@ -1,10 +1,15 @@
 package com.revakovsky.thenytimesbooks.core
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
-open class BaseViewModel : ViewModel() {
+open class BaseViewModel(
+    private val connectivityObserver: ConnectivityObserver,
+) : ViewModel() {
 
     protected val _connectedToTheInternet = MutableStateFlow<Boolean?>(null)
     val connectedToTheInternet = _connectedToTheInternet.asStateFlow()
@@ -18,6 +23,16 @@ open class BaseViewModel : ViewModel() {
     protected val _errorMessage = MutableStateFlow("")
     val errorMessage = _errorMessage.asStateFlow()
 
+
+    protected fun checkConnectivity() {
+        if (!connectivityObserver.hasConnection()) _connectedToTheInternet.tryEmit(false)
+        connectivityObserver.observeConnectivity().onEach { connectivityStatus ->
+            when (connectivityStatus) {
+                ConnectivityObserver.Status.Available -> _hasConnectivity.tryEmit(true)
+                else -> _hasConnectivity.tryEmit(false)
+            }
+        }.launchIn(viewModelScope)
+    }
 
     open fun resetStates() {
         _connectedToTheInternet.value = null
--- app/src/main/java/com/revakovsky/thenytimesbooks/presentation/screens/categories/CategoryViewModel.kt
+++ app/src/main/java/com/revakovsky/thenytimesbooks/presentation/screens/categories/CategoryViewModel.kt
@@ -11,15 +11,13 @@ import com.revakovsky.thenytimesbooks.utils.mapToCategoryUi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class CategoryViewModel @Inject constructor(
-    private val connectivityObserver: ConnectivityObserver,
+    connectivityObserver: ConnectivityObserver,
     private val getBookCategoriesUseCase: GetBookCategoriesUseCase,
-) : BaseViewModel() {
+) : BaseViewModel(connectivityObserver) {
 
     private val _categories = MutableStateFlow<List<CategoryUi>>(emptyList())
     val categories = _categories.asStateFlow()
@@ -27,16 +25,6 @@ class CategoryViewModel @Inject constructor(
     init {
         checkConnectivity()
         getCategories(shouldUpdateBooksInfo = false)
-    }
-
-    private fun checkConnectivity() {
-        if (!connectivityObserver.hasConnection()) _connectedToTheInternet.tryEmit(false)
-        connectivityObserver.observeConnectivity().onEach { connectivityStatus ->
-            when (connectivityStatus) {
-                ConnectivityObserver.Status.Available -> _hasConnectivity.tryEmit(true)
-                else -> _hasConnectivity.tryEmit(false)
-            }
-        }.launchIn(viewModelScope)
     }
 
     fun getCategories(shouldUpdateBooksInfo: Boolean) {
